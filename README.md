2d0fe2a117c90ca0cbe884a602ced011d452c519
edmond buck <edmond.buck@wearetissue.com>
2023-01-07 03:00:32 -0800

    completed readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # calender-work-day-scheduler
+Work Day Scheduler
+The purpose of this project was to create a daily planner to help effectively manage time.
+This daily planner features the Day.js library along with dynamically updating the HTML and CSS files with the jQuery framework.
+
+The daily planner also utilizes the users local storage by saving the events inputted by the user. So if the page is every refreshed the user will still be able to access their planned events.
+
+Below are a some examples of the daily planner in action.
--- develop/script.js
+++ develop/script.js
@@ -8,35 +8,35 @@ $(function () {
   
     // click function on button 
     saveBtn.on('click', function () {
-      $(this).siblings('.description').get(0).value;
-      let calenderText = $(this).siblings('.description').get(0).value;
-      localStorage.setItem($(this).parent().attr('id'), calenderText);
-    });
-  
-    // array of hours of the work day
-    let hoursOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-  
-    for (i = 0; i < hoursOfDay.length; i++) {
-      let eventStorage = localStorage.getItem('hour-' + hoursOfDay[i]);
-      let hourTextArea = $('#hour-' + hoursOfDay[i] + ' > .description');
-      hourTextArea.textContent = eventStorage;
-    }
-  
-    // variable of current hour to be able to add past present and future class
-    let currentHour = dayjs().hour();
-  
-    // for loop to add past present and future class to days of week
-    for (var i = 0; i < hoursOfDay.length; i ++) {
-      if (hoursOfDay[i] < currentHour) {
-        $('#hour-' + hoursOfDay[i]).addClass('past');
-      } else if (hoursOfDay[i] == currentHour) {
-        $('#hour-' + hoursOfDay[i]).removeClass('past');
-        $('#hour-' + hoursOfDay[i]).addClass('present');
-      } else {
-        $('#hour-' + hoursOfDay[i]).removeClass('past');
-        $('#hour-' + hoursOfDay[i]).removeClass('present');
-        $('#hour-' + hoursOfDay[i]).addClass('future');
+        $(this).siblings('.description').get(0).value;
+        let calenderText = $(this).siblings('.description').get(0).value;
+        localStorage.setItem($(this).parent().attr('id'), calenderText);
+      });
+    
+      // array of hours of the work day
+      let hoursOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+    
+      for (i = 0; i < hoursOfDay.length; i++) {
+        let eventStorage = localStorage.getItem('hour-' + hoursOfDay[i]);
+        let hourTextArea = $('#hour-' + hoursOfDay[i] + ' > .description');
+        hourTextArea.textContent = eventStorage;
       }
-    }
+    
+      // variable of current hour to be able to add past present and future class
+      let currentHour = dayjs().hour();
+    
+      // for loop to add past present and future class to days of week
+      for (var i = 0; i < hoursOfDay.length; i ++) {
+        if (hoursOfDay[i] < currentHour) {
+          $('#hour-' + hoursOfDay[i]).addClass('past');
+        } else if (hoursOfDay[i] == currentHour) {
+          $('#hour-' + hoursOfDay[i]).removeClass('past');
+          $('#hour-' + hoursOfDay[i]).addClass('present');
+        } else {
+          $('#hour-' + hoursOfDay[i]).removeClass('past');
+          $('#hour-' + hoursOfDay[i]).removeClass('present');
+          $('#hour-' + hoursOfDay[i]).addClass('future');
+        }
+    };
   });
-  Footer
+  
